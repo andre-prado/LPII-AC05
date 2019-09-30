d2167ae45797df0f1eaa1130428ad6db33ad05f5
AndrÚ Pereira Prado <1900986@SALAS.AULAS>
2019-09-30 11:14:39 -0300

    classe Cachorro criada

--- animais.py
+++ animais.py
@@ -2,3 +2,12 @@ from mamifero import Mamifero
 from reptil import Reptil
 
 # cavalo, cachorro, gato / Cobra Jacare
+
+
+class Cachorro(Mamifero):
+    def __init__(self, nome, cor_pelo, idade, tipo_pata):
+        super().__init__(nome, cor_pelo, idade, tipo_pata)
+        self.nome = nome
+        self.cor_pelo = cor_pelo
+        self.idade = idade
+        self.tipo_pata = tipo_pata
