df687c1a76acc8d41b045bd9a1b0f36700c79622
Andre Prado <andr3prad0@gmail.com>
2019-09-30 22:03:41 -0300

    classe Gato pronta

--- animais.py
+++ animais.py
@@ -18,3 +18,23 @@ class Cachorro(Mamifero):
 
     def rosnar(self):
         return '{} está rosnando'.format(self.nome)
+
+
+class Gato(Mamifero):
+    def __init__(self, nome, cor_pelo, idade, tipo_pata):
+        super().__init__(nome, cor_pelo, idade, tipo_pata)
+        self.nome = nome
+        self.cor_pelo = cor_pelo
+        self.idade = idade
+        self.tipo_pata = tipo_pata
+        self.vida = 7
+
+    def miar(self):
+        return '{} está miando'.format(self.nome)
+
+    def morrer(self):
+        if self.vida == 0:
+            return '{} morreu'.format(self.nome)
+        else:
+            self.vida -= 1
+            return '{} tem {} vida(s) sobrando'.format(self.nome, self.vida)
--- teste.py
+++ teste.py
@@ -1,6 +1,20 @@
-from mamifero import Mamifero
-from animais import Cachorro
+from animais import Gato
 
+"""
 marley = Cachorro('Marley', 'marrom', 2, 'peluda', 'Akita')
 print(marley.latir())
 print(marley.rosnar())
+"""
+
+
+mimi = Gato('Mimi', 'Cinza', 4, 'sem pelo')
+print(mimi.nome)
+print(mimi.morrer())
+print(mimi.morrer())
+print(mimi.morrer())
+print(mimi.morrer())
+print(mimi.morrer())
+print(mimi.morrer())
+print(mimi.miar())
+print(mimi.morrer())
+print(mimi.morrer())
