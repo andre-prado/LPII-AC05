65d3a37fcb586706415a34c05e15b3802e45d200
Andre Prado <andr3prad0@gmail.com>
2019-09-30 21:49:59 -0300

    classe filha cachorro pronta

--- animais.py
+++ animais.py
@@ -5,9 +5,16 @@ from reptil import Reptil
 
 
 class Cachorro(Mamifero):
-    def __init__(self, nome, cor_pelo, idade, tipo_pata):
+    def __init__(self, nome, cor_pelo, idade, tipo_pata, raca):
         super().__init__(nome, cor_pelo, idade, tipo_pata)
         self.nome = nome
         self.cor_pelo = cor_pelo
         self.idade = idade
         self.tipo_pata = tipo_pata
+        self.raca = raca
+
+    def latir(self):
+        return '{} está latindo'.format(self.nome)
+
+    def rosnar(self):
+        return '{} está rosnando'.format(self.nome)
---
+++ teste.py
@@ -0,0 +1,6 @@
+from mamifero import Mamifero
+from animais import Cachorro
+
+marley = Cachorro('Marley', 'marrom', 2, 'peluda', 'Akita')
+print(marley.latir())
+print(marley.rosnar())
